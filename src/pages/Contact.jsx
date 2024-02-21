f1f67f59680afcd312698c32433f60b2ad3b20da
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-02-22 00:47:12 +0300

    Contact Page tamamlandı.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,18 +15,20 @@ import {
 import contactImg from "../assets/contact/contactimg.png";
 const Contact = () => {
   return (
-    <div className="flex flex-col text-center  font-montserrat font-bold px-40 py-0 pb-10 gap-12">
+    <div className="flex flex-col text-center  font-montserrat font-bold px-40 py-0 pb-10 gap-12 sm:px-0 sm:py-10">
       <section
         id="contact-us"
-        className="flex flex-row justify-between items-center"
+        className="flex flex-row sm:flex-col justify-between items-center sm:gap-12 sm:px-10"
       >
         <span
           id="text-box"
-          className="flex flex-col text-darkblue1 gap-12 items-start"
+          className="flex flex-col text-darkblue1 gap-12 items-start sm:items-center"
         >
           <h3 className="h4-styles">CONTACT US</h3>
-          <h1 className="text-5xl w-3/4 text-start">Get in touch today!</h1>
-          <h3 className="h4-styles text-gray w-3/4 text-start">
+          <h1 className="text-5xl w-3/4 text-start sm:text-center sm:w-full">
+            Get in touch today!
+          </h1>
+          <h3 className="h4-styles text-gray w-3/4 text-start sm:text-center sm:w-full">
             We know how large objects will act, but things on a small scale
           </h3>
           <h4 className="h3-styles text-2xl">Phone ; +451 215 215 </h4>
@@ -55,15 +57,18 @@ const Contact = () => {
       </section>
       <section
         id="get-support"
-        className="flex flex-col gap-12 items-center w-full"
+        className="flex flex-col gap-12 items-center w-full sm:px-10"
       >
-        <span id="texts" className="flex flex-col gap-5 w-3/5">
+        <span id="texts" className="flex flex-col gap-5 w-3/5 sm:w-full ">
           <h5 className="h5-styles text-darkblue1">VISIT OUR OFFICE</h5>
-          <h2 className="h2-styles items-center">
+          <h2 className="h2-styles items-center sm:text-4xl">
             We help small businesses with big ideas
           </h2>
         </span>
-        <span id="boxes" className="flex flex-row justify-center w-4/5">
+        <span
+          id="boxes"
+          className="flex flex-row justify-center w-4/5 sm:flex-col sm:w-full"
+        >
           <span id="box-1" className="box-style border-none">
             <FontAwesomeIcon icon={faPhone} className="text-6xl text-blue1" />
             <p className="p-styles w-full">georgia.young@example.com</p>
