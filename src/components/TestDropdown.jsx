ab166b765f98e84f8b8c54df896bff31a4c23107
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-05-26 03:17:22 +0300

    Shop butonu düzenlendi.

--- src/components/TestDropdown.jsx
+++ src/components/TestDropdown.jsx
@@ -33,27 +33,29 @@ const TestDropdown = () => {
     >
       <div className="relative flex text-left">
         <div>
-          <button
-            className="inline-flex w-full justify-center gap-x-1.5 text-sm text-gray-900"
-            id="menu-button"
-            aria-expanded={isOpen}
-            aria-haspopup="true"
-            onClick={toggleMenu}
-          >
-            Shop
-            <svg
-              className="h-5 w-5 text-gray-900"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              aria-hidden="true"
+          <Link to="/shop">
+            <button
+              className="inline-flex w-full justify-center gap-x-1.5 text-sm text-gray-900"
+              id="menu-button"
+              aria-expanded={isOpen}
+              aria-haspopup="true"
+              onClick={toggleMenu}
             >
-              <path
-                fillRule="evenodd"
-                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
+              Shop
+              <svg
+                className="h-5 w-5 text-gray-900"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </Link>
         </div>
         {isOpen && (
           <div
