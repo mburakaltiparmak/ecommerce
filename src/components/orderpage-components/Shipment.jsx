d5ed92ece01f282e9581e7aed0e926575b4d9dc2
burakaltiparmak <mburakaltiparmak@gmail.com>
2024-07-08 04:43:05 +0300

    Shipment page tamamlandı. shippingCartAction içinde store'dan çekmek için shipping price oluşturulacak.

--- src/components/orderpage-components/Shipment.jsx
+++ src/components/orderpage-components/Shipment.jsx
@@ -1,7 +1,18 @@
 const Shipment = () => {
   return (
-    <div className="bg-darkblue1 border rounded-xl text-white px-8 flex flex-col py-4 w-full">
-      Shipment
+    <div className="bg-darkblue1 border rounded-xl text-white px-8 flex flex-col py-4 w-full font-montserrat h-fit">
+      <select
+        className="text-darkblue1 font-bold h-10 w-full border border-blue1 rounded-md"
+        defaultValue="standart"
+        id="shipment-dropdown"
+      >
+        <option disabled value="">
+          Select Shipment Method
+        </option>
+        <option value="standart">Standart Shipment : 29.99$</option>
+        <option value="urgent">Urgent Shipment : 50.00$</option>
+        <option value="special">Special Shipment : 75.00$</option>
+      </select>
     </div>
   );
 };
